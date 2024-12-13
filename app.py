193fdb3002c0c72de0b8b548da9182be579c84d3
tzj <2530755229@qq.com>
2024-12-13 14:54:24 +0800

    enable server.js

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
+
+# 启动 server.js
+def run_server_js():
+    subprocess.Popen(['node', 'static/js/server.js'])
 
 @app.route('/')
 def main_page():
@@ -11,4 +16,5 @@ def info_page():
     return render_template('info.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    run_server_js()  # 在启动 Flask 应用之前运行 server.js
+    app.run()
